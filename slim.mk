1f3c3541e78a037ec88a7204e68b9e42896c5f79
Nicholas Flintham <nick@flinny.org>
2013-11-20 17:24:33 +0000

    m7vzw: access vzw apps in Play store
    Change-Id: I1603fc286cc4241c73a7e020363a117da8350972

--- slim.mk
+++ slim.mk
@@ -6,7 +6,13 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=HTCOneVZW BUILD_ID=JDQ39 BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=HTCOneVZW \
+    BUILD_ID=JDQ39 \
+    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.2.2/JDQ39/221778.10:user/release-keys" \
+    PRIVATE_BUILD_DESC="1.10.605.10 CL221778 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 PRODUCT_NAME := slim_m7vzw
 PRODUCT_DEVICE := m7vzw
